94d0ba4e9294a9afcbab2d2509534ac8ed51e626
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-10-15 12:39:51 +0900

    プレイヤーの移動を実装

--- Player.h
+++ Player.h
@@ -18,12 +18,39 @@ private:
 	/// </summary>
 	KamataEngine::Model* model_ = nullptr;
 
+	//速度
+	KamataEngine::Vector3 velocity_ = {};
+	//加速度定数
+	static inline const float kAcceleration = 0.001f;
+	//移動減衰定数
+	static inline const float kAttenuation = 0.05f;
+	//速度制限
+	static inline const float kMaxSpeed = 3.0f;
+
+	//接地状態のフラグ
+	bool onGround_ = true;
+	//重力加速度定数
+	static inline const float kGravityAcceleration = 0.03f;
+	//最大落下速度定数
+	static inline const float kMaxFallSpeed = 5.0f;
+	//ジャンプ初速度定数
+	static inline const float kJumpAcceleration = 0.5f;
+
 public:
-	//初期化
-	void Initialize(KamataEngine::Model* model, const KamataEngine::Vector3& position);
-	//更新
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model"></param>
+	/// <param name="camera"></param>
+	/// <param name="position"></param>
+	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& position);
+	/// <summary>
+	/// 更新
+	/// </summary>
 	void Update();
-	//描画
+	/// <summary>
+	/// 描画
+	/// </summary>
 	void Draw();
 
 };
---
+++ WorldTransformUpdate.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"KamataEngine.h"
+
+/// <summary>
+/// ワールドトランスフォームの更新
+/// </summary>
+void WorldTransformUpdate(KamataEngine::WorldTransform &worldTransform);
